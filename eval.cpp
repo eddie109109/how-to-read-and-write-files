cf3c29b114d13334a21349dedec409b1f3c6533a
eddie109109 <43190484+eddie109109@users.noreply.github.com>
2019-02-19 09:12:22 -0700

    Add files via upload

---
+++ eval.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>   // we need this to do read file and write file
+#include <string>
+
+using namespace std;
+
+
+
+
+int main() {
+   ifstream infile;
+
+   infile.open("C:/Users/Eddie/Desktop/path.txt");
+   if (infile.fail()) {
+    cerr << "error opening file" << endl;
+    exit(1);
+   } 
+
+   int x,y;
+
+   infile >> x >> y;
+
+   cout << x << endl;
+   cout << y << endl;
+
+   infile.close();
+
+   return 0;
+}
+
+
+int main() {
+   ifstream infile;
+
+   infile.open("C:/Users/Eddie/Desktop/path.txt");
+   if (infile.fail()) {
+    cerr << "error opening file" << endl;
+    exit(1);
+   } 
+
+   string item;
+   int counter = 0;
+
+   while (!infile.eof()){
+        infile >> item;
+        counter++;
+   }
+   cout<< counter << " items found" <<endl;
+
+   infile.close();
+
+
+   return 0;
+}
+
+
+int main() {
+    ofstream outfile;
+
+    outfile.open("C:/Users/Eddie/Desktop/path.txt");  // if the file name doesn't exisit, it will be automatically created for us
+
+    outfile<< "hi";
+
+    outfile.cloe();  // very important
+
+    return 0;
+}
